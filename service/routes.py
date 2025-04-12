f2cebaba5868bf0793ca52549d51546293b22ba6
Jonathan-AJ <60198234+Jonathan-AJ@users.noreply.github.com>
2025-04-12 17:29:40 +0300

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -38,6 +38,8 @@ def index():
 ######################################################################
 # CREATE A NEW ACCOUNT
 ######################################################################
+
+
 @app.route("/accounts", methods=["POST"])
 def create_accounts():
     """
@@ -58,6 +60,8 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################    
+
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -73,6 +77,8 @@ def list_accounts():
 ######################################################################
 # READ AN ACCOUNT
 ###################################################################### 
+
+
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """
@@ -88,6 +94,8 @@ def get_accounts(account_id):
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
+
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
@@ -105,6 +113,8 @@ def update_accounts(account_id):
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
+
+
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
     """
