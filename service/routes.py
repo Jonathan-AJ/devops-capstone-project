9480cd4ea75d5cf1ae0293c9cb64bfb122f9e15b
Jonathan-AJ <60198234+Jonathan-AJ@users.noreply.github.com>
2025-04-12 17:23:13 +0300

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -60,10 +60,10 @@ def create_accounts():
 ######################################################################    
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
-        """
-        List all Accounts
-        This endpoint will list all Accounts
-        """
+    """
+    List all Accounts
+    This endpoint will list all Accounts
+    """
         app.logger.info("Request to list Accounts")
         accounts = Account.all()
         account_list = [account.serialize() for account in accounts]
@@ -75,10 +75,10 @@ def list_accounts():
 ###################################################################### 
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
-        """
-        Reads an Account
-        This endpoint will read an Account based the account_id that is requested
-        """
+    """
+    Reads an Account
+    This endpoint will read an Account based the account_id that is requested
+    """
         app.logger.info("Request to read an Account with id: %s", account_id)
         account = Account.find(account_id)
         if not account:
@@ -107,10 +107,10 @@ def update_accounts(account_id):
 ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
-        """
-        Delete an Account
-        This endpoint will delete an Account based on the account_id that is requested
-        """
+    """
+    Delete an Account
+    This endpoint will delete an Account based on the account_id that is requested
+    """
         app.logger.info("Request to delete an Account with id: %s", account_id)
         account = Account.find(account_id)
         if account:
